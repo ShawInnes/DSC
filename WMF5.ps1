134d02ae548825d5ccad5450480ff7a0f62a92e2
Shaw Innes <shaw@immortal.net.au>
2015-03-23 08:26:05 +1000

    Update WMF5.ps1

--- WMF5.ps1
+++ WMF5.ps1
@@ -1,4 +1,4 @@
-﻿if ((Get-CimInstance -ClassName Win32_OperatingSystem).Version -eq '6.3.9600')
+if ((Get-CimInstance -ClassName Win32_OperatingSystem).Version -eq '6.3.9600')
 {
     if ((Get-Ciminstance -ClassName Win32_Processor).Addresswidth -eq '64') 
     {
@@ -9,15 +9,19 @@
         $url = 'http://download.microsoft.com/download/5/5/2/55277C4B-75D1-40FB-B99C-4EAFA249F645/WindowsBlue-KB2894868-x86.msu'
     }
   
+    Write-Output 'Downloading Installer'
+
     $fileName = Split-Path $url -Leaf
     $downloadPath = "$env:userprofile\Downloads\$fileName"
     $webClient = New-Object System.Net.WebClient
     $webClient.DownloadFile($url,$downloadPath)
   
+    Write-Output 'Executing Installer'
     Invoke-Expression -Command "wusa.exe $downloadPath /quiet /norestart"  
+    Write-Output 'Installation Complete'
 }
 else 
 {
-    Write-Warning -message 'Windows Management Framework 5.0 requires Windows 8.1 or Windows Server 2012 R2'
+    Write-Output 'Windows Management Framework 5.0 requires Windows 8.1 or Windows Server 2012 R2'
 } 
 
